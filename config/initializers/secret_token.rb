a5c33c00bb673ef906f45ea0fc066a75ba807aea
Juan Duran <jcduran05@gmail.com>
2013-12-23 23:22:46 +0900

    Created a rails application for practice. Create status using scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPracticeSns::Application.config.secret_token = 'ec2a64ebade6c588342553d8f16c2c920ae647c70b2c41cef0bd773fffc8a61c97800e0c0c7dd6c4e82de2b23732897469880fbcc9f0f52a182858b841a2cf7e'
